23392f31c9a17fb2c24c1e6d038b365fb91efe3e
Lunarstrain82 <artur@agriland.ie>
2021-11-30 17:34:21 +0000

    extracted comment component

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -51,27 +51,10 @@
                 </div>
             </div>
 
-            <section class="col-span-8 col-start-5">
-                <article class="flex bg-gray-200 p-6 rounded-lg border border-gray-300">
-                    <div class="flex-shrink-0">
-                        <img src="https://i.pravatar.cc/70" height="70" width="70" alt="Random Image" class="rounded-lg">
-                    </div>
-                    <div class="ml-6">
-                        <header>
-                            <h2 class="text-xl">Artur Torres</h2>
-                            <p class="text-xs text-gray-500 italic">
-                                <time>
-                                    Posted 4 months ago
-                                </time>
-                            </p>
-                        </header>
-                        <p>
-                            Comment goes here. There is more editorial staff coming in soon.
-                            Tomorrow we are going to face the worst ever thunderstorm.
-                            Why not to take this seriously and hide somewhere in the woods?
-                        </p>
-                    </div>
-                </article>
+            <section class="col-span-8 col-start-5 space-y-6">
+                <x-post-comment />
+                <x-post-comment />
+                <x-post-comment />
             </section>
         </article>
     </main>
