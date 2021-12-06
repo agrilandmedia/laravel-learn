1f39ce3645faab1717ef518353d17f4ec5c757a2
Lunarstrain82 <artur@agriland.ie>
2021-12-06 12:41:54 +0000

    done latest cleaning

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -2,6 +2,6 @@
 
 <x-form.field-wrapper>
     <x-form.label name="{{ $name }}" />
-    <input class="border border-gray-400 p-2 w-full rounded-md" type="{{ $type }}" name="{{ $name }}" id="{{ $name }}" {{ $attributes(['value' => old($name)]) }}>
+    <input class="border border-gray-400 p-2 w-full rounded-md" type="{{ $type }}" name="{{ $name }}" id="{{ $name }}" {{ $attributes(['value' => old($name)]) }} />
     <x-form.error name="{{ $name }}" />
 </x-form.field-wrapper>
--- resources/views/components/form/textarea.blade.php
+++ resources/views/components/form/textarea.blade.php
@@ -2,6 +2,6 @@
 
 <x-form.field-wrapper>
     <x-form.label name="{{ $name }}" />
-    <textarea name="{{ $name }}" cols="25" rows="5" class="border border-gray-400 w-full rounded-md" {{ $attributes }}>{{ $slot ?? old($name) }}</textarea>
+    <textarea name="{{ $name }}" cols="25" rows="5" class="border border-gray-400 p-2 w-full rounded-md" {{ $attributes }}>{{ $slot ?? old($name) }}</textarea>
     <x-form.error name="{{ $name }}" />
 </x-form.field-wrapper>
